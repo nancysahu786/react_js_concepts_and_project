2a832dd271030aecaa8e94d501a6c399cb90bff0
unknown <nancysahu111222@gmail.com>
2025-03-02 13:06:19 +0530

    delete task

--- 03_useEffect/vite-project/src/Components/Todo/HandleAllTodo.jsx
+++ 03_useEffect/vite-project/src/Components/Todo/HandleAllTodo.jsx
@@ -3,16 +3,26 @@ import InputBox from './InputBox';
 import List from './List';
 
 function HandleAllTodo() {
-    const [taskArr,setTaskArr] = useState([]);
-    const addTask = (inputValue) => {
-        const newValue = inputValue;
-        const newTaskArr = [...taskArr,newValue];
-        setTaskArr(newTaskArr);
-    }
+  const [taskArr, setTaskArr] = useState([]);
+  const addTask = (inputValue) => {
+    const newValue = inputValue;
+    const newTaskArr = [...taskArr, newValue];
+    setTaskArr(newTaskArr);
+  }
+
+  const deleteTask = (value) => {
+    console.log("delete index", value);
+    const newTaskArr = taskArr.filter((task, index) => { return index != value }
+
+    );
+    console.log("newTaskArr", newTaskArr);
+    // console.log("taskArr",taskArr);
+    setTaskArr(newTaskArr);
+  }
   return (
     <div>
-      <InputBox addTask={addTask}/>
-      <List taskArr={taskArr}/>
+      <InputBox addTask={addTask} />
+      <List taskArr={taskArr} deleteTask={deleteTask} />
     </div>
   )
 }
--- 03_useEffect/vite-project/src/Components/Todo/List.jsx
+++ 03_useEffect/vite-project/src/Components/Todo/List.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
 
 function List(props) {
-    const {taskArr} = props;
+    const { taskArr, deleteTask } = props;
 
     const onDeleteItem = (value) => {
-        console.log("value",value);
-        
+        console.log("value", value);
+        deleteTask(value);
     }
-  return (
-    <>
-    <div className='list'>
-      <ul>
-        {
-            taskArr.map(((task,index)=>{
-                return <>
-                <li key={index}>{task}</li>
-                <button onClick={() => onDeleteItem(index)}>Delete</button>
-                </>
-
-            
-}))
-        }
-      </ul>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className='list'>
+                <ul>
+                    {
+                        taskArr.map(((task, index) => {
+                            return <>
+                                <li key={index}>{task}</li>
+                                <button onClick={() => onDeleteItem(index)}>Delete</button>
+                            </>
+                        }))
+                    }
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default List
